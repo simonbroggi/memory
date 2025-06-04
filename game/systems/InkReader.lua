fdaaed562dc83a3cba8d093b10cb474b7609049e
Simon Broggi <simon.broggi@insert-coin.ch>
2025-06-04 12:17:55 +0200

    choice buttons

--- game/systems/InkReader.lua
+++ game/systems/InkReader.lua
@@ -7,6 +7,9 @@ local InkReader = {
 function InkReader:init()
     local story_definition = import("ink_story.story_main")
     self.story = Story(story_definition)
+
+    -- add a choice button pool
+    self.choiceButtonPool = {}
 end
 
 function InkReader:update(dt)
@@ -53,7 +56,48 @@ function InkReader:presentChoices(choices)
         if #c.tags > 0 then
             print(" # tags: " .. table.concat(c.tags, ", "))
         end
+        self:showChoice(c, i)
     end
+end
+
+function InkReader:hideChoices()
+    -- hide all choice buttons
+    for _, button in ipairs(self.choiceButtonPool) do
+        core.ecs_world.entities:remove(button)
+    end
+end
+
+
+function InkReader:showChoice(choice, index)
+    local button
+    if #self.choiceButtonPool >= index then
+        button = self.choiceButtonPool[index]
+        core.ecs_world.entities:add(button)
+    else
+        -- create a new choice button
+        button = core.newEntitytInWorld()
+        table.insert(self.choiceButtonPool, button) -- add to the pool
+        button.tform = {x = 1100, y = 300+index * 60}
+        button.ui = true
+        button.rectangle = {width=200, height=50}
+        button.material = {red=0, green=0, blue=1, alpha=0.7}
+        button.textbox = {
+            font = love.graphics.newFont(20),
+            text = choice.text,
+            limit = 200,
+            ox = 100,
+            oy = 25,
+            align = "center",
+        }
+        button.choiceIndex = index
+        button.onPointerDown = function(entity)
+            self.story:ChooseChoiceIndex(entity.choiceIndex) -- choose the choice at the index
+            self:hideChoices() -- hide all choices after selecting one
+        end
+    end
+    button.tform.y = 300 + index * 60
+    button.textbox.text = choice.text
+
 end
 
 return InkReader
